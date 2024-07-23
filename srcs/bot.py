0be6043833593627b0f838763ac117adea8eca1b
gabrielclcardoso <gabrielclcardoso@gmail.com>
2024-07-23 14:56:53 -0300

    feat: add scheduled task skeleton

--- srcs/bot.py
+++ srcs/bot.py
@@ -10,19 +10,36 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+JOBS = []
+
 
 def main():
     application = Application.builder().token(const.TOKEN).build()
 
-    hello_handler = CommandHandler('hello', hello)
-    application.add_handler(hello_handler)
+    job_handler = CommandHandler('add', add_job)
+    application.add_handler(job_handler)
+
+    job_queue = application.job_queue
+    job_queue.run_repeating(print_jobs, interval=5)
 
     application.run_polling()
 
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   text="Hello World!")
+async def add_job(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if str(update.effective_chat.id) != const.CHAT_ID:
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text="Unauthorized")
+
+    for job in context.args:
+        JOBS.append(job)
+        await context.bot.send_message(chat_id=const.CHAT_ID,
+                                       text=f'added {job}')
+
+
+async def print_jobs(context: ContextTypes.DEFAULT_TYPE):
+    for job in JOBS:
+        await context.bot.send_message(chat_id=const.CHAT_ID, text=job)
+
 
 if __name__ == '__main__':
     main()
